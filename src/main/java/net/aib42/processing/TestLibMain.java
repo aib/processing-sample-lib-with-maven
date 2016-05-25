eaee49d9aa1ca98a8b5485f08232b93b90d2b9ea
aib <aibok42@gmail.com>
2016-05-25 11:35:57 +0300

    Add PApplet handling to main library class

--- src/main/java/net/aib42/processing/TestLibMain.java
+++ src/main/java/net/aib42/processing/TestLibMain.java
@@ -1,11 +1,30 @@
 package net.aib42.processing;
 
+import processing.core.PApplet;
+
 public class TestLibMain {
+	private PApplet papplet;
+
 	public TestLibMain() {
 		System.out.println("TestLibMain()");
+		this.papplet = null;
+	}
+
+	public TestLibMain(PApplet papplet) {
+		this.papplet = papplet;
+		papplet.println("TestLibMain loaded");
 	}
 
 	public int test() {
 		return 42;
+	}
+
+	public void printSketchInfo() {
+		if (papplet != null) {
+			papplet.println(
+				"Sketch, size " + papplet.width + "x" + papplet.height + ", " +
+				"is using renderer " + papplet.sketchRenderer()
+			);
+		}
 	}
 }
